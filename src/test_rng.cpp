53aefd15c2cddf81fe562cc22a4f01091952e9f8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-06-10 19:16:25 +0100

    Utility testing

--- src/test_rng.cpp
+++ src/test_rng.cpp
@@ -1,10 +1,11 @@
 #include <dust/rng.hpp>
+#include <dust/util.hpp>
 
 #include "test_rng.h"
 
 extern "C" SEXP test_rng(SEXP r_n, SEXP r_seed) {
-  int n = INTEGER(r_n)[0];
-  int seed = INTEGER(r_seed)[0];
+  int n = dust::util::as_size(r_n, "n");
+  int seed = dust::util::as_size(r_seed, "seed");
 
   const int nt = 1, index = 0;
   const double mean = 0, sd = 1;
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,33 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% NULL, NULL)
   expect_equal(NULL %||% 2, 2)
 })
+
+
+test_that("as_integer/as_size (C++)", {
+  expect_error(
+    .Call(Ctest_rng, 100, "string"),
+    "Expected an integer for 'seed'")
+  expect_error(
+    .Call(Ctest_rng, 100, 1.5),
+    "Expected an integer for 'seed' (rounding error?)", fixed = TRUE)
+  expect_error(
+    .Call(Ctest_rng, 100, 1:5),
+    "Expected a scalar for 'seed'", fixed = TRUE)
+  expect_error(
+    .Call(Ctest_rng, 100, -1),
+    "Expected a non-negative integer for 'seed'", fixed = TRUE)
+})
+
+
+test_that("check pointer", {
+  p <- .Call(Ctest_walk_alloc, 1, 10L, 1L)
+  null <- unserialize(serialize(p, NULL))
+  expect_error(
+    .Call(Ctest_walk_run, null, 1L),
+    "Pointer has been invalidated (perhaps serialised?)",
+    fixed = TRUE)
+  expect_error(
+    .Call(Ctest_walk_run, NULL, 1L),
+    "Expected an external pointer",
+    fixed = TRUE)
+})
