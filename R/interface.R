6ec474347637ea99a930220cc7e2caab68e09135
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-06-11 09:40:23 +0100

    Hack to keep R CMD check happy

--- R/interface.R
+++ R/interface.R
@@ -19,3 +19,14 @@ dust <- function(alloc, run, classname = "dust") {
       }
     ))
 }
+
+
+## The checking code looks for the objects in the package namespace, so defining
+## dll here removes the following NOTE
+## Registration problem:
+##   Evaluating 'private$cpp_alloc' during check gives error
+## 'object 'private' not found':
+##    .Call(private$cpp_alloc, ...)
+## See https://github.com/wch/r-source/blob/d4e8fc9832f35f3c63f2201e7a35fbded5b5e14c/src/library/tools/R/QC.R#L1950-L1980
+private <- list(cpp_alloc = structure(list(), class = "NativeSymbolInfo"),
+                cpp_run = structure(list(), class = "NativeSymbolInfo"))
