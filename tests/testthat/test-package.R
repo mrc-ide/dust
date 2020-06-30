3a73202148ad4b46d1db86cebcded7ba57cf4d21
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-06-30 10:34:28 +0100

    Start of package testing

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -2,19 +2,9 @@ context("package")
 
 test_that("validate package", {
   skip_if_not_installed("pkgload")
-  path <- tempfile()
-  dir.create(path)
-  dir.create(file.path(path, "inst/dust"), FALSE, TRUE)
 
-  name <- "pkg"
-
-  data <- list(name = name)
-  writeLines(glue_whisker(read_lines("examples/pkg/DESCRIPTION"), data),
-             file.path(path, "DESCRIPTION"))
-  writeLines(glue_whisker(read_lines("examples/pkg/NAMESPACE"), data),
-             file.path(path, "NAMESPACE"))
-  file.copy(dust_file("examples/walk.cpp"), file.path(path, "inst/dust"))
-  file.copy(dust_file("examples/sir.cpp"), file.path(path, "inst/dust"))
+  path <- create_test_package()
+  file.remove(file.path(path, "src")) # ensure we create this as needed
 
   path <- dust_package(path)
 
@@ -25,4 +15,36 @@ test_that("validate package", {
   rm(w)
   gc()
   pkgload::unload("pkg")
+})
+
+
+test_that("validate package dependencies", {
+  deps <- data.frame(
+    type = c("LinkingTo", "LinkingTo", "Imports"),
+    package = c("Rcpp", "dust", "Rcpp"),
+    version = "*",
+    stringsAsFactors = FALSE)
+  expect_silent(package_validate_has_dep(deps, "Rcpp", "LinkingTo"))
+  expect_silent(package_validate_has_dep(deps, "Rcpp", "Imports"))
+  expect_error(
+    package_validate_has_dep(deps, "Rcpp", "Depends"),
+    "Expected package 'Rcpp' as 'Depends' in DESCRIPTION")
+  expect_error(
+    package_validate_has_dep(deps, "other", "Imports"),
+    "Expected package 'other' as 'Imports' in DESCRIPTION")
+})
+
+
+test_that("validate destination notices existing C++ code", {
+  msg <- "File '.+\\.cpp' does not look like it was created by dust - stopping"
+  path <- create_test_package()
+  file.create(file.path(path, "src", "walk.cpp"))
+  expect_error(
+    package_validate_destination(path, c("sir.cpp", "walk.cpp")),
+    msg)
+
+  writeLines("// some actual content", file.path(path, "src", "walk.cpp"))
+  expect_error(
+    package_validate_destination(path, c("sir.cpp", "walk.cpp")),
+    msg)
 })
