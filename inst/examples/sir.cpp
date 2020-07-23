693a70fd6830b54a1a872708dd642ce3d22a9c7d
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-23 20:14:56 +0100

    Revert changes

--- inst/examples/sir.cpp
+++ inst/examples/sir.cpp
@@ -1,13 +1,13 @@
 class sir {
 public:
-  typedef float real_t;
+  typedef double real_t;
   struct init_t {
-    real_t S0;
-    real_t I0;
-    real_t R0;
-    real_t beta;
-    real_t gamma;
-    real_t dt;
+    double S0;
+    double I0;
+    double R0;
+    double beta;
+    double gamma;
+    double dt;
   };
 
   sir(const init_t& data) : data_(data) {
@@ -17,26 +17,23 @@ public:
     return 3;
   }
 
-  std::vector<real_t> initial(size_t step) {
-    std::vector<real_t> ret = {data_.S0, data_.I0, data_.R0};
+  std::vector<double> initial(size_t step) {
+    std::vector<double> ret = {data_.S0, data_.I0, data_.R0};
     return ret;
   }
 
-#ifdef __NVCC__
-  __device__
-#endif
-  void update(size_t step, const std::vector<real_t>& state,
-              dust::rng_state_t<real_t>& rng_state,
-              std::vector<real_t>& state_next) {
-    real_t S = state[0];
-    real_t I = state[1];
-    real_t R = state[2];
-    real_t N = S + I + R;
+  void update(size_t step, const std::vector<double>& state,
+              dust::rng_state_t<double>& rng_state,
+              std::vector<double>& state_next) {
+    double S = state[0];
+    double I = state[1];
+    double R = state[2];
+    double N = S + I + R;
 
-    real_t p_SI = 1 - std::exp(-(data_.beta) * I / N);
-    real_t p_IR = 1 - std::exp(-(data_.gamma));
-    real_t n_IR = dust::distr::rbinom(rng_state, I, p_IR * data_.dt);
-    real_t n_SI = dust::distr::rbinom(rng_state, S, p_SI * data_.dt);
+    double p_SI = 1 - std::exp(-(data_.beta) * I / N);
+    double p_IR = 1 - std::exp(-(data_.gamma));
+    double n_IR = dust::distr::rbinom(rng_state, I, p_IR * data_.dt);
+    double n_SI = dust::distr::rbinom(rng_state, S, p_SI * data_.dt);
 
     state_next[0] = S - n_SI;
     state_next[1] = I + n_SI - n_IR;
@@ -51,25 +48,25 @@ private:
 template <>
 sir::init_t dust_data<sir>(cpp11::list data) {
   // Initial state values
-  real_t I0 = 10.0;
-  real_t S0 = 1000.0;
-  real_t R0 = 0.0;
+  double I0 = 10.0;
+  double S0 = 1000.0;
+  double R0 = 0.0;
 
   // Default rates
-  real_t beta = 0.2;
-  real_t gamma = 0.1;
+  double beta = 0.2;
+  double gamma = 0.1;
 
   // Time scaling
-  real_t dt = 0.25;
+  double dt = 0.25;
 
   // Accept beta and gamma as optional elements
   SEXP r_beta = data["beta"];
   if (r_beta != R_NilValue) {
-    beta = cpp11::as_cpp<real_t>(r_beta);
+    beta = cpp11::as_cpp<double>(r_beta);
   }
   SEXP r_gamma = data["gamma"];
   if (r_gamma != R_NilValue) {
-    gamma = cpp11::as_cpp<real_t>(r_gamma);
+    gamma = cpp11::as_cpp<double>(r_gamma);
   }
 
   return sir::init_t{S0, I0, R0, beta, gamma, dt};
