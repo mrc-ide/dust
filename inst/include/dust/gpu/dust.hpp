bff60a706ee0b1b88690f3d4f14e1d0df0b5ad63
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-23 19:36:37 +0100

    Drop template for now

--- inst/include/dust/gpu/dust.hpp
+++ inst/include/dust/gpu/dust.hpp
@@ -15,7 +15,6 @@
 // TODO: For reasons unknown, this does not get found on raplab
 // #include <cub/device/device_select.cuh>
 
-template <typename T>
 __global__
 void run_particles(T** models,
                   real_t** particle_y,
