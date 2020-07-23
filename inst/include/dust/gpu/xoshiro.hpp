0ad96d8dac19e3298344cc9d45c0b44f0f3ddba6
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-23 19:30:41 +0100

    Remove stray i

--- inst/include/dust/gpu/xoshiro.hpp
+++ inst/include/dust/gpu/xoshiro.hpp
@@ -112,7 +112,7 @@ inline double device_unif_rand(rng_state_t<T>& state) {
   return rand;
 }
 
-template <typename T>i
+template <typename T>
 __device__
 nline float device_unif_randf(rng_state_t<T>& state) {
   return(__double2float_rn(device_unif_rand(state)));
