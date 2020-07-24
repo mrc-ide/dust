05ef7f1256c2a350db3c0c9957f546efba0eab2a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-24 08:03:46 +0100

    Use cpp11 exceptions, not exit, for cuda errors

--- inst/include/dust/gpu/cuda.cuh
+++ inst/include/dust/gpu/cuda.cuh
@@ -5,29 +5,25 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <cpp11/protect.hpp>
 
-//Cuda call
-static void HandleCUDAError(const char *file,
-	int line,
-	cudaError_t status = cudaGetLastError()) {
+static void HandleCUDAError(const char *file, int line,
+                            cudaError_t status = cudaGetLastError()) {
 #ifdef _DEBUG
-	cudaDeviceSynchronize();
+  cudaDeviceSynchronize();
 #endif
-    if (status != CUDA_SUCCESS || (status = cudaGetLastError()) != CUDA_SUCCESS)
-    {
-        if (status == cudaErrorUnknown)
-        {
-            printf("%s(%i) An Unknown CUDA Error Occurred :(\n", file, line);
-            getchar();
-            exit(1);
-        }
-        printf("%s(%i) CUDA Error Occurred;\n%s\n", file, line, cudaGetErrorString(status));
-        getchar();
-        exit(1);
+
+  if (status != CUDA_SUCCESS || (status = cudaGetLastError()) != CUDA_SUCCESS) {
+    if (status == cudaErrorUnknown) {
+      cpp11::stop("%s(%i) An Unknown CUDA Error Occurred :(\n",
+                  file, line);
     }
+    cpp11::stop("%s(%i) CUDA Error Occurred;\n%s\n",
+                file, line, cudaGetErrorString(status));
+  }
 }
+
 #define CUDA_CALL( err ) (HandleCUDAError(__FILE__, __LINE__ , err))
 #define CUDA_CHECK() (HandleCUDAError(__FILE__, __LINE__))
-
 
 #endif
