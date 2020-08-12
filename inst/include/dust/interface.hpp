c72e74436c745c7bd422dcddb916eb3d475a7e42
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-08-12 17:28:26 +0100

    No longer broken for floats

--- inst/include/dust/interface.hpp
+++ inst/include/dust/interface.hpp
@@ -55,7 +55,6 @@ cpp11::writable::doubles dust_simulate(cpp11::sexp r_steps,
                                        const size_t seed) {
   typedef typename T::real_t real_t;
   std::vector<size_t> steps = validate_size(r_steps, "steps");
-  // TODO: broken for floats
   std::vector<real_t> state = matrix_to_vector<real_t>(r_state);
   std::vector<size_t> index = r_index_to_index(r_index, r_state.nrow());
 
