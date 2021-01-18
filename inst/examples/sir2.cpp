1fc3da6be943bec4b46bc8fe13148efc34e1fc61
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-01-18 12:42:58 +0000

    Move sir2 to use more real_t and less double

--- inst/examples/sir2.cpp
+++ inst/examples/sir2.cpp
@@ -1,4 +1,5 @@
-double ldpois(double x, double lambda) {
+template <typename real_t>
+real_t ldpois(real_t x, real_t lambda) {
   return x * std::log(lambda) - lambda - std::lgamma(x + 1);
 }
 
@@ -6,19 +7,19 @@ class sir2 {
 public:
   typedef double real_t;
   struct init_t {
-    double S0;
-    double I0;
-    double R0;
-    double beta;
-    double gamma;
-    double dt;
+    real_t S0;
+    real_t I0;
+    real_t R0;
+    real_t beta;
+    real_t gamma;
+    real_t dt;
     size_t freq;
     // Observation parameters
-    double exp_noise;
+    real_t exp_noise;
   };
   // Presence of this is going to be important
   struct data_t {
-    double incidence;
+    real_t incidence;
   };
 
   sir2(const init_t& pars) : pars_(pars) {
@@ -28,25 +29,25 @@ public:
     return 5; // S, I, R, cum_inc, inc
   }
 
-  std::vector<double> initial(size_t step) {
-    std::vector<double> ret = {pars_.S0, pars_.I0, pars_.R0, 0, 0};
+  std::vector<real_t> initial(size_t step) {
+    std::vector<real_t> ret = {pars_.S0, pars_.I0, pars_.R0, 0, 0};
     return ret;
   }
 
   void update(size_t step, const real_t * state,
               dust::rng_state_t<real_t>& rng_state,
               real_t * state_next) {
-    double S = state[0];
-    double I = state[1];
-    double R = state[2];
-    double cumulative_incidence = state[3];
+    real_t S = state[0];
+    real_t I = state[1];
+    real_t R = state[2];
+    real_t cumulative_incidence = state[3];
 
-    double N = S + I + R;
+    real_t N = S + I + R;
 
-    double p_SI = 1 - std::exp(-(pars_.beta) * I / N);
-    double p_IR = 1 - std::exp(-(pars_.gamma));
-    double n_IR = dust::distr::rbinom(rng_state, I, p_IR * pars_.dt);
-    double n_SI = dust::distr::rbinom(rng_state, S, p_SI * pars_.dt);
+    real_t p_SI = 1 - std::exp(-(pars_.beta) * I / N);
+    real_t p_IR = 1 - std::exp(-(pars_.gamma));
+    real_t n_IR = dust::distr::rbinom(rng_state, I, p_IR * pars_.dt);
+    real_t n_SI = dust::distr::rbinom(rng_state, S, p_SI * pars_.dt);
 
     state_next[0] = S - n_SI;
     state_next[1] = I + n_SI - n_IR;
@@ -59,9 +60,9 @@ public:
 
   real_t compare_data(const real_t * state, const data_t& data,
                       dust::rng_state_t<real_t>& rng_state) {
-    const double incidence_modelled = state[4];
-    const double incidence_observed = data.incidence;
-    const double lambda = incidence_modelled +
+    const real_t incidence_modelled = state[4];
+    const real_t incidence_observed = data.incidence;
+    const real_t lambda = incidence_modelled +
       dust::distr::rexp(rng_state, pars_.exp_noise);
     return ldpois(incidence_observed, lambda);
   }
@@ -79,24 +80,25 @@ inline double with_default(double default_value, cpp11::sexp value) {
 
 template <>
 sir2::init_t dust_pars<sir2>(cpp11::list pars) {
+  typedef sir2::real_t real_t;
   // Initial state values
-  double I0 = 10.0;
-  double S0 = 1000.0;
-  double R0 = 0.0;
+  real_t I0 = 10.0;
+  real_t S0 = 1000.0;
+  real_t R0 = 0.0;
 
   // Rates, which can be set based on the provided pars
   // [[dust::param(beta, required = FALSE)]]
-  double beta = with_default(0.2, pars["beta"]);
+  real_t beta = with_default(0.2, pars["beta"]);
   // [[dust::param(gamma, required = FALSE)]]
-  double gamma = with_default(0.1, pars["gamma"]);
+  real_t gamma = with_default(0.1, pars["gamma"]);
 
   // Time scaling
   size_t freq = 4;
-  double dt = 1.0 / static_cast<double>(freq);
+  real_t dt = 1.0 / static_cast<real_t>(freq);
 
   // Compare function
   // [[dust::param(exp_noise, required = FALSE)]]
-  double exp_noise = with_default(1e6, pars["exp_noise"]);
+  real_t exp_noise = with_default(1e6, pars["exp_noise"]);
 
   return sir2::init_t{S0, I0, R0, beta, gamma, dt, freq, exp_noise};
 }
