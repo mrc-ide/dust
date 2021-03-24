3c8918c197bdf7d8f352dafc4bb709069da9dc82
John Lees <lees.john6@gmail.com>
2021-03-24 16:13:27 +0000

    Copy the filter classes for CUDA

--- inst/include/dust/filter.hpp
+++ inst/include/dust/filter.hpp
@@ -72,6 +72,90 @@ std::vector<typename T::real_t> filter(Dust<T> * obj,
   return log_likelihood;
 }
 
+
+template <typename T>
+std::vector<typename T::real_t> filter_device(Dust<T> * obj,
+                                       filter_state_device<typename T::real_t>& state
+                                       bool save_trajectories,
+                                       std::vector<size_t> step_snapshot) {
+  typedef typename T::real_t real_t;
+
+  const size_t n_particles = obj->n_particles();
+  const size_t n_data = obj->n_data();
+  const size_t n_pars = obj->n_pars_effective();
+  const size_t n_particles_each = n_particles / n_pars;
+  dust::device_array<real_t> log_likelihood(n_pars);
+  dust::device_array<real_t> log_likelihood_step(n_pars);
+  dust::device_array<real_t> weights(n_particles);
+  dust::device_array<size_t> kappa(n_particles);
+
+  // NOTES
+  // 1) state.trajectories: this is a device_array which copies state (y_selected)
+  // at each step. order needs kappa copied in at each step
+  // 2) state.snapshots: this is a device_array which copies state_full at some
+  // steps
+  // 1 & 2) both of these will then be deinterleaved in their class
+  // 3) obj->run: obj->run_device should just work
+  // 4) obj->compare_data: will be like run_particles, but data needs to be flattened
+  // and copied to the device. d.second will then be a pointer in to this
+  // 5) scale_log_weights(): use cub::DeviceSegmentedReduce::Max and a simple kernel which calls exp()
+  // NB: this is only used to calculate the total likelihood
+  // 6) resample: calls resample_weight with a single RNG draw
+  // see https://github.com/mrc-ide/mcstate/blob/master/R/particle_filter.R#L426-L431
+  // or https://github.com/NVIDIA/cuda-samples/blob/master/Samples/shfl_scan/shfl_scan.cu
+  // for prefix sum version
+  // or https://arxiv.org/pdf/1301.4019.pdf for rejection sampler version
+
+
+  if (save_trajectories) {
+    state.trajectories.resize(obj->n_state(), n_particles, n_data);
+    obj->state(state.trajectories.value_iterator());
+    state.trajectories.advance();
+  }
+
+  state.snapshots.resize(obj->n_state_full(), n_particles, step_snapshot);
+
+  for (auto & d : obj->data()) {
+    obj->run(d.first);
+    obj->compare_data(weights, d.second);
+
+    // TODO: we should cope better with the case where all weights
+    // are 0; I think that is the behaviour in the model (or rather
+    // the case where there is no data and so we do not resample)
+    //
+    // TODO: we should cope better with the case where one filter
+    // has become impossible but others continue, but that's hard!
+    auto wi = weights.begin();
+    for (size_t i = 0; i < n_pars; ++i) {
+      log_likelihood_step[i] = scale_log_weights<real_t>(wi, n_particles_each);
+      log_likelihood[i] += log_likelihood_step[i];
+      wi += n_particles_each;
+    }
+
+    // We could move this below if wanted but we'd have to rewrite
+    // the re-sort algorithm; that would be worth doing I think
+    // https://github.com/mrc-ide/dust/issues/202
+    if (save_trajectories) {
+      obj->state(state.trajectories.value_iterator());
+    }
+
+    obj->resample(weights, kappa);
+
+    if (save_trajectories) {
+      std::copy(kappa.begin(), kappa.end(),
+                state.trajectories.order_iterator());
+      state.trajectories.advance();
+    }
+
+    if (state.snapshots.is_snapshot_step(d.first)) {
+      obj->state_full(state.snapshots.value_iterator());
+      state.snapshots.advance();
+    }
+  }
+
+  return log_likelihood;
+}
+
 }
 }
 
--- inst/include/dust/filter_state.hpp
+++ inst/include/dust/filter_state.hpp
@@ -100,6 +100,100 @@ private:
 };
 
 template <typename real_t>
+class filter_trajectories_device {
+public:
+  filter_trajectories_device() {
+  }
+
+  void resize(size_t n_state, size_t n_particles, size_t n_data) {
+    n_state_ = n_state;
+    n_particles_ = n_particles;
+    n_data_ = n_data;
+    offset_ = 0;
+    history_value = dust::device_array<real_t>(n_state_ * n_particles_ * (n_data_ + 1));
+    history_order = dust::device_array<size_t>(n_particles_ * (n_data_ + 1));
+    std::vector<size_t> index_init(n_particles_);
+    std::iota(index_init.begin(), index_init.end(), 0);
+    CUDA_CALL(cudaMemcpyAsync(history_order.data(), index_init.data(),
+                              n_particles_ * sizeof(size_t), cudaMemcpyDefault))
+  }
+
+  typename std::vector<real_t>::iterator value_iterator() {
+    return history_value.data() + offset_ * n_state_ * n_particles_;
+  }
+
+  typename std::vector<size_t>::iterator order_iterator() {
+    return history_order.data() + offset_ * n_particles_;
+  }
+
+  std::vector<real_t> history() const {
+    std::vector<real_t> ret(size());
+    history(ret.begin());
+    return ret;
+  }
+
+  // This is a particularly unpleasant bit of bookkeeping and is
+  // adapted from mcstate (see the helper files in tests for a
+  // translation of the the code). As we proceed we store the values
+  // of particles *before* resampling and then we store the index used
+  // in resampling. We do not resample all the history at each
+  // resample as that is prohibitively expensive.
+  //
+  // So to output sensible history we start with a particle and we
+  // look to see where it "came from" in the previous step
+  // (history_index) and propagate this backward in time to
+  // reconstruct what is in effect a multifurcating tree.
+  // This is analogous to the particle ancestor concept in the
+  // particle filter literature.
+  //
+  // It's possible we could do this more efficiently for some subset
+  // of particles too (give me the history of just one particle) by
+  // breaking the function before the loop over 'k'.
+  //
+  // Note that we treat history_order and history_value as read-only
+  // though this process so one could safely call this multiple times.
+
+  // TODO: rewrite this to deal with interleaved state
+  // TODO: may be best to write this as a kernel instead
+  template <typename Iterator>
+  void history(Iterator ret) const {
+    std::vector<size_t> index_particle(n_particles_);
+    for (size_t i = 0; i < n_particles_; ++i) {
+      index_particle[i] = i;
+    }
+    for (size_t k = 0; k < n_data_ + 1; ++k) {
+      size_t i = n_data_ - k;
+      auto const it_order = history_order.begin() + i * n_particles_;
+      auto const it_value = history_value.begin() + i * n_state_ * n_particles_;
+      auto it_ret = ret + i * n_state_ * n_particles_;
+      for (size_t j = 0; j < n_particles_; ++j) {
+        const size_t idx = *(it_order + index_particle[j]);
+        index_particle[j] = idx;
+        std::copy_n(it_value + idx * n_state_, n_state_,
+                    it_ret + j * n_state_);
+      }
+    }
+  }
+
+  size_t size() const {
+    return history_value.size();
+  }
+
+  void advance() {
+    offset_++;
+  }
+
+private:
+  size_t n_state_;
+  size_t n_particles_;
+  size_t n_data_;
+  size_t offset_;
+  size_t len_;
+  dust::device_array<real_t> history_value;
+  dust::device_array<size_t> history_order;
+};
+
+template <typename real_t>
 class filter_snapshots {
 public:
   filter_snapshots() {
@@ -145,9 +239,62 @@ private:
 };
 
 template <typename real_t>
+class filter_snapshots_device {
+public:
+  filter_snapshots_device() {
+  }
+
+  void resize(size_t n_state, size_t n_particles, std::vector<size_t> steps) {
+    n_state_ = n_state;
+    n_particles_ = n_particles;
+    n_steps_ = steps.size();
+    offset_ = 0;
+    steps_ = steps;
+    state_ = dust::device_array<real_t>(n_state_ * n_particles_ * n_steps_);
+  }
+
+  bool is_snapshot_step(size_t step) {
+    return offset_ < n_steps_ && steps_[offset_] == step;
+  }
+
+  typename std::vector<real_t>::iterator value_iterator() {
+    return state_.data() + offset_ * n_state_ * n_particles_;
+  }
+
+  void advance() {
+    offset_++;
+  }
+
+  size_t size() const {
+    return state_.size();
+  }
+
+  // TODO: deinterleave this when it is written out (see dust refresh_state)
+  // or write a kernel to do so and then just use a memcpy
+  template <typename Iterator>
+  void history(Iterator dest) const {
+    std::copy(state_.begin(), state_.end(), dest);
+  }
+
+private:
+  size_t n_state_;
+  size_t n_particles_;
+  size_t n_steps_;
+  size_t offset_;
+  dust::device_array<size_t> steps_;
+  dust::device_array<real_t> state_;
+};
+
+template <typename real_t>
 struct filter_state {
   filter_trajectories<real_t> trajectories;
   filter_snapshots<real_t> snapshots;
+};
+
+template <typename real_t>
+struct filter_state_device {
+  filter_trajectories_device<real_t> trajectories;
+  filter_snapshots_device<real_t> snapshots;
 };
 
 }
