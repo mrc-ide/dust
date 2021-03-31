ec287203d6fe5938e6a96a991bf9f06a5ed6953b
John Lees <lees.john6@gmail.com>
2021-03-31 16:06:09 +0100

    Fix int->real_t in prefix scan

--- inst/include/dust/prefix_scan.cuh
+++ inst/include/dust/prefix_scan.cuh
@@ -13,17 +13,18 @@ const int scan_block_size = 128;
 // during consecutive scan operations.
 namespace dust {
 
+template <typename real_t>
 struct BlockPrefixCallbackOp
 {
     // Running prefix
-    int running_total;
+    real_t running_total;
     // Constructor
-    DEVICE BlockPrefixCallbackOp(int running_total) : running_total(running_total) {}
+    DEVICE BlockPrefixCallbackOp(real_t running_total) : running_total(running_total) {}
     // Callback operator to be entered by the first warp of threads in the block.
     // Thread-0 is responsible for returning a value for seeding the block-wide scan.
-    DEVICE int operator()(int block_aggregate)
+    DEVICE real_t operator()(real_t block_aggregate)
     {
-        int old_prefix = running_total;
+        real_t old_prefix = running_total;
         running_total += block_aggregate;
         return old_prefix;
     }
@@ -36,18 +37,18 @@ KERNEL void prefix_scan(real_t * cum_weights,
   const size_t n_particles_each = n_particles / n_pars;
   const int par_idx = blockIdx.x;
   // Specialize BlockScan for a 1D block of 128 threads
-  typedef cub::BlockScan<int, scan_block_size> BlockScan;
+  typedef cub::BlockScan<real_t, scan_block_size> BlockScan;
   // Allocate shared memory for BlockScan
   __shared__ typename BlockScan::TempStorage temp_storage;
   // Initialize running total
-  BlockPrefixCallbackOp prefix_op(0);
+  BlockPrefixCallbackOp prefix_op<real_t>(0);
 
   // Have the block iterate over segments of items
   for (int block_offset = par_idx * n_particles_each;
        block_offset < (par_idx + 1) * n_particles_each;
        block_offset += scan_block_size) {
     // Load a segment of consecutive items that are blocked across threads
-    int thread_data = cum_weights[block_offset];
+    real_t thread_data = cum_weights[block_offset];
     // Collectively compute the block-wide exclusive prefix sum
     BlockScan(temp_storage).InclusiveSum(
         thread_data, thread_data, prefix_op);
@@ -55,6 +56,7 @@ KERNEL void prefix_scan(real_t * cum_weights,
     // Store scanned items to output segment
     cum_weights[block_offset] = thread_data;
   }
+  __syncthreads();
 }
 
 }
