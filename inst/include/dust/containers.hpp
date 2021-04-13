518275885ad6949b7f285077521cac02aed3456e
John Lees <lees.john6@gmail.com>
2021-04-13 18:11:06 +0100

    Add async memcpy to filter state

--- inst/include/dust/containers.hpp
+++ inst/include/dust/containers.hpp
@@ -119,6 +119,20 @@ public:
 #endif
   }
 
+  void get_array(T * dst, cuda_stream& stream, const bool async = false) const {
+#ifdef __NVCC__
+    if (async) {
+      CUDA_CALL(cudaMemcpyAsync(dst, data_, dst.size() * sizeof(T),
+                          cudaMemcpyDefault, stream.stream()));
+    } else {
+      CUDA_CALL(cudaMemcpy(dst, data_, dst.size() * sizeof(T),
+                          cudaMemcpyDefault));
+    }
+#else
+    std::memcpy(dst, data_, dst.size() * sizeof(T));
+#endif
+  }
+
   // General method to set the device array, allowing src to be written
   // into the device data_ array starting at dst_offset
   void set_array(const T* src, const size_t src_size,
