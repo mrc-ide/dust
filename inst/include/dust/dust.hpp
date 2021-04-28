e29aebdeb38414e665319d133a88638c00b285b5
John Lees <lees.john6@gmail.com>
2021-04-28 15:32:40 +0100

    Fix dodgy merge

--- inst/include/dust/dust.hpp
+++ inst/include/dust/dust.hpp
@@ -413,7 +413,8 @@ public:
                 dust::device_scan_state<real_t>& scan) {
     refresh_device();
     dust::filter::run_device_resample(n_particles(), n_pars_effective(), n_state_full(),
-                                      cuda_pars_, rng_.state(n_particles()),
+                                      cuda_pars_, kernel_stream_, resample_stream_,
+                                      rng_.state(n_particles()),
                                       device_state_, weights, scan);
     select_needed_ = true;
   }
