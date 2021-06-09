cb96cfeb94ee58188f3e003fb092960750f725a4
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-09 13:44:42 +0100

    More robust resampling

--- inst/include/dust/filter_tools.hpp
+++ inst/include/dust/filter_tools.hpp
@@ -12,16 +12,20 @@ void resample_weight(typename std::vector<real_t>::const_iterator w,
                      size_t n, real_t u, size_t offset,
                      typename std::vector<size_t>::iterator idx) {
   const real_t tot = std::accumulate(w, w + n, static_cast<real_t>(0));
-  real_t ww = 0.0, uu = tot * u / n, du = tot / n;
-
+  real_t ww = 0.0, uu0 = tot * u / n, du = tot / n;
   size_t j = offset;
   for (size_t i = 0; i < n; ++i) {
-    while (ww < uu) {
+    // We could accumulate uu by adding du at each iteration but that
+    // suffers roundoff error here with floats.
+    const real_t uu = uu0 * i * du;
+    // The second clause should never be hit but prevents any invalid
+    // read if we have pathalogical 'u' that is within floating point
+    // eps of 1
+    while (ww < uu && j < n) {
       ww += *w;
       ++w;
       ++j;
     }
-    uu += du;
     *idx = j == 0 ? 0 : j - 1;
     ++idx;
   }
