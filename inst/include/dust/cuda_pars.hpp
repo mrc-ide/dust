cb55d8db3d4f78cd2c1cab75a3ee21aed71dcaeb
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-06-23 10:59:23 +0100

    Simplify

--- inst/include/dust/cuda_pars.hpp
+++ inst/include/dust/cuda_pars.hpp
@@ -7,95 +7,40 @@ namespace dust {
 //   return (n_particles * len + block_size - 1) / block_size;
 // }
 
-template <typename real_t>
-cuda_launch_control cuda_pars_run(size_t n_particles, size_t n_particles_each,
-                                  size_t n_shared_int, size_t n_shared_real,
-                                  size_t shared_size) {
-  cuda_launch_control ret;
-
-  const size_t n_pars_effective = n_particles / n_particles_each;
-  const int warp_size = dust::cuda::warp_size;
-  size_t warp_block_size =
-    warp_size * (n_particles_each + warp_size - 1) / warp_size;
-
-  size_t n_shared_int_effective = n_shared_int +
-    dust::utils::align_padding(n_shared_int * sizeof(int),
-                               sizeof(real_t)) / sizeof(int);
-  size_t shared_size_int_bytes = n_shared_int_effective * sizeof(int);
-  size_t shared_size_both_bytes =
-    shared_size_int_bytes + n_shared_real * sizeof(real_t);
-
-  if (n_particles_each < warp_size) {
-    ret.shared_int = false;
-    ret.shared_real = false;
-    ret.shared_size_bytes = 0;
-  } else {
-    ret.shared_int = shared_size_int_bytes <= shared_size;
-    ret.shared_real = shared_size_both_bytes <= shared_size;
-    if (ret.shared_real) {
-      ret.shared_size_bytes = shared_size_both_bytes;
-    } else if (ret.shared_int) {
-      ret.shared_size_bytes = shared_size_int_bytes;
-    } else {
-      ret.shared_size_bytes = 0;
-    }
-  }
-
-  ret.block_size = 128;
-  if (ret.shared_int || ret.shared_real) {
-    // Either (or both) int and real will fit into shared (L1
-    // cache), each block runs a pars set. Each pars set has enough
-    // blocks to run all of its particles, the final block may have
-    // some threads that don't do anything (hang off the end)
-    ret.block_size = std::min(ret.block_size, warp_block_size);
-    ret.block_count =
-      n_pars_effective * (n_particles_each + ret.block_size - 1) /
-      ret.block_size;
-  } else {
-    // If not enough particles per pars to make a whole block use
-    // shared, or if shared_t too big for L1, turn it off, and run
-    // in 'classic' mode where each particle is totally independent
-    // ret.block_size = 128;
-    ret.block_count = (n_particles + ret.block_size - 1) / ret.block_size;
-  }
-
-  return ret;
-}
-
-
-// Compare kernel
+// Run / Compare kernel
 //
-// Almost the same as above, but we must also cope with the size of
-// the data element too.
-template <typename real_t>
-cuda_launch_control cuda_pars_compare(size_t n_particles,
-                                      size_t n_particles_each,
-                                      size_t n_shared_int,
-                                      size_t n_shared_real,
-                                      size_t data_size,
-                                      size_t shared_size) {
-  cuda_launch_control ret;
+// For the compare version we must also cope with the size of the data
+// element too.
+inline cuda_launch_control launch_control_model(size_t n_particles,
+                                                size_t n_particles_each,
+                                                size_t n_shared_int,
+                                                size_t n_shared_real,
+                                                size_t real_size,
+                                                size_t data_size,
+                                                size_t shared_size) {
+  const size_t int_size = sizeof(int);
 
   const size_t n_pars_effective = n_particles / n_particles_each;
   const int warp_size = dust::cuda::warp_size;
-  size_t warp_block_size =
+  const size_t warp_block_size =
     warp_size * (n_particles_each + warp_size - 1) / warp_size;
-  size_t n_shared_int_effective = n_shared_int +
-    dust::utils::align_padding(n_shared_int * sizeof(int),
-                               sizeof(real_t)) / sizeof(int);
-  size_t shared_size_int_bytes = n_shared_int_effective * sizeof(int);
+  const size_t n_shared_int_effective = n_shared_int +
+    dust::utils::align_padding(n_shared_int * int_size,
+                               real_size) / int_size;
+  const size_t shared_size_int_bytes = n_shared_int_effective * int_size;
 
-  size_t real_align = data_size == 0 ? sizeof(real_t) : 16;
-  size_t n_shared_real_effective = n_shared_real +
+  const size_t real_align = data_size == 0 ? real_size : 16;
+  const size_t n_shared_real_effective = n_shared_real +
     dust::utils::align_padding(shared_size_int_bytes +
-                               n_shared_real * sizeof(real_t),
-                               real_align) / sizeof(real_t);
+                               n_shared_real * real_size,
+                               real_align) / real_size;
 
-  size_t shared_size_both_bytes =
+  const size_t shared_size_both_bytes =
     shared_size_int_bytes +
-    n_shared_real_effective * sizeof(real_t) +
+    n_shared_real_effective * real_size +
     data_size;
 
+  cuda_launch_control ret;
   if (n_particles_each < warp_size) {
     ret.shared_int = false;
     ret.shared_real = false;
@@ -135,28 +80,28 @@ cuda_launch_control cuda_pars_compare(size_t n_particles,
 
 
 // TODO: rename!
-template <typename real_t>
-cuda_launch_dust set_cuda_pars(int device_id,
-                               size_t n_particles,
-                               size_t n_particles_each,
-                               size_t n_state,
-                               size_t n_state_full,
-                               size_t n_shared_int,
-                               size_t n_shared_real,
-                               size_t data_size,
-                               size_t shared_size) {
+inline cuda_launch_dust set_cuda_pars(int device_id,
+                                      size_t n_particles,
+                                      size_t n_particles_each,
+                                      size_t n_state,
+                                      size_t n_state_full,
+                                      size_t n_shared_int,
+                                      size_t n_shared_real,
+                                      size_t real_size,
+                                      size_t data_size,
+                                      size_t shared_size) {
   if (device_id < 0) {
     return cuda_launch_dust{};
   }
 
   cuda_launch_dust cuda_pars;
 
-  cuda_pars.run = cuda_pars_run<real_t>(n_particles, n_particles_each,
-                                        n_shared_int, n_shared_real,
-                                        shared_size);
-  cuda_pars.compare = cuda_pars_compare<real_t>(n_particles, n_particles_each,
-                                                n_shared_int, n_shared_real,
-                                                data_size, shared_size);
+  cuda_pars.run = launch_control_model(n_particles, n_particles_each,
+                                       n_shared_int, n_shared_real,
+                                       real_size, 0, shared_size);
+  cuda_pars.compare = launch_control_model(n_particles, n_particles_each,
+                                           n_shared_int, n_shared_real,
+                                           real_size, data_size, shared_size);
 
   // Reorder kernel
   cuda_pars.reorder.block_size = 128;
--- src/test_cuda_pars.cpp
+++ src/test_cuda_pars.cpp
@@ -18,11 +18,12 @@ SEXP test_cuda_pars(int device_id, int n_particles, int n_particles_each,
                     int shared_size) {
 
   // n_pars_effective can be worked out from n_particles and each
-
-  auto pars = dust::set_cuda_pars<float>(device_id, n_particles, n_particles_each,
-                                         n_state, n_state_full,
-                                         n_shared_int, n_shared_real, data_size,
-                                         shared_size);
+  size_t real_size = sizeof(float);
+  auto pars = dust::set_cuda_pars(device_id, n_particles, n_particles_each,
+                                  n_state, n_state_full,
+                                  n_shared_int, n_shared_real,
+                                  real_size, data_size,
+                                  shared_size);
 
   using namespace cpp11::literals;
   return cpp11::writable::list({"run"_nm = launch_r_list(pars.run),
