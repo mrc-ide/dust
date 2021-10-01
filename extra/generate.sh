0491a848e1238c98806f7b708bdce3e21afc1a15
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-10-01 16:04:53 +0100

    Generate some reference data from upstream rng

---
+++ extra/generate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p bin out src
+cat targets | while read target
+do
+    file="${target}.c"
+    src="src/${file}"
+    if [[ ! -f $src ]]; then
+        echo "Downloading $src"
+        (cd src; curl -sSLO "https://prng.di.unimi.it/${file}")
+    fi
+    exe="bin/$target"
+    dest="out/$target.txt"
+    def=$(sed -E 's/(xo.*[0-9]+).*/\U\1/' <<< $target)
+    echo "Compiling $exe"
+    g++ -include $src -D$def -o$exe harness.cpp
+    echo "Generating $dest"
+    ./$exe > $dest
+done
---
+++ extra/harness.cpp
@@ -0,0 +1,58 @@
+#include <array>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+
+#if defined(XOSHIRO256)
+typedef uint64_t data_type;
+constexpr size_t data_size = 4;
+#elif defined(XOSHIRO128)
+typedef uint64_t data_type;
+constexpr size_t data_size = 4;
+#elif defined(XOROSHIRO128)
+typedef uint64_t data_type;
+constexpr size_t data_size = 4;
+#else
+#error "no target defined"
+#endif
+
+uint64_t splitmix64(uint64_t seed) {
+  uint64_t z = (seed += 0x9e3779b97f4a7c15);
+  z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+  z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+  return z ^ (z >> 31);
+}
+
+template <typename T, size_t N>
+std::array<T, N> rng_seed(uint64_t seed) {
+  std::array<T, N> state;
+  const size_t n = N;
+  for (size_t i = 0; i < n; ++i) {
+    seed = splitmix64(seed);
+    state[i] = static_cast<data_type>(seed);
+  }
+  return state;
+}
+
+int main() {
+  std::array<data_type, data_size> seed = rng_seed<data_type, data_size>(42);
+  for (size_t i = 0; i < data_size; ++i) {
+    s[i] = seed[i];
+  }
+  constexpr int n = 10;
+  std::array<data_type, n * 3> ret;
+  for (int i = 0; i < n * 3; ++i) {
+    if (i == n - 1) {
+      jump();
+    } else if (i == 2 * n - 1) {
+      long_jump();
+    }
+    ret[i] = next();
+    std::cout <<
+      std::setw(16) << std::setfill('0') << std::hex << ret[i] <<
+      " " << std::dec << ret[i] <<
+      std::endl;
+  }
+
+  return 0;
+}
