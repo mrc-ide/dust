c0eed418e486014dcc43c80f5212d54c9409b7d0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-10-18 14:25:42 +0100

    WIP

--- inst/include/dust/cuda/dust_device.hpp
+++ inst/include/dust/cuda/dust_device.hpp
@@ -54,7 +54,7 @@ public:
     // errors_(n_particles_total_),
     shape_({n_particles}),
     device_config_(device_config) {
-    // initialise(pars, step, n_particles, true);
+    initialise(pars, step, n_particles, true);
     // initialise_index();
   }
 
@@ -90,6 +90,71 @@ private:
   // errors
   std::vector<size_t> shape_; // shape of output
   cuda::device_config device_config_;
+
+  // Device support
+  dust::cuda::launch_control_dust cuda_pars_; // TODO => control_ ?
+  dust::cuda::device_state<real_type, rng_state_type> device_state_;
+  dust::cuda::device_array<data_type> device_data_;
+  std::map<size_t, size_t> device_data_offsets_;
+  dust::cuda::cuda_stream kernel_stream_;
+  dust::cuda::cuda_stream resample_stream_;
+  bool select_needed_;
+  bool select_scatter_;
+  size_t device_step_;
+
+  void initialise(const pars_type& pars, const size_t step,
+                  const size_t n_particles, bool set_state) {
+    const size_t n = particles_.size() == 0 ? 0 : n_state_full();
+    dust::Particle<T> p(pars, step);
+    if (n > 0 && p.size() != n) {
+      std::stringstream msg;
+      msg << "'pars' created inconsistent state size: " <<
+        "expected length " << n << " but created length " <<
+        p.size();
+      throw std::invalid_argument(msg.str());
+    }
+
+    if (particles_.size() == n_particles) {
+#ifdef _OPENMP
+      #pragma omp parallel for schedule(static) num_threads(n_threads_)
+#endif
+      for (size_t i = 0; i < n_particles; ++i) {
+        particles_[i].set_pars(p, set_state);
+      }
+      shared_[0] = pars.shared;
+    } else {
+      particles_.clear();
+      particles_.reserve(n_particles);
+      for (size_t i = 0; i < n_particles; ++i) {
+        particles_.push_back(p);
+      }
+      shared_ = {pars.shared};
+      initialise_device_state();
+    }
+    // reset_errors();
+    update_device_shared();
+
+    const size_t n_shared_int = device_state_.n_shared_int;
+    const size_t n_shared_real = device_state_.n_shared_real;
+    std::vector<int> shared_int(n_shared_int * n_pars_effective());
+    std::vector<real_type> shared_real(n_shared_real * n_pars_effective());
+    for (size_t i = 0; i < shared_.size(); ++i) {
+      int * dest_int = shared_int.data() + n_shared_int * i;
+      real_type * dest_real = shared_real.data() + n_shared_real * i;
+      dust::cuda::device_shared_copy<T>(shared_[i], dest_int, dest_real);
+    }
+    device_state_.shared_int.set_array(shared_int);
+    device_state_.shared_real.set_array(shared_real);
+    
+    set_cuda_launch();
+
+    device_step_ = step;
+    stale_host_ = false;
+    stale_device_ = true;
+    select_needed_ = true;
+  }
+
+  
 };
 
 // } ?
