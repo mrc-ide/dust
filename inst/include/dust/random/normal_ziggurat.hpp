338a9a34121b70b929b18c6e0924a0baaffaee6a
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-11-08 10:23:17 +0000

    Make code more gpu-friendly

--- inst/include/dust/random/normal_ziggurat.hpp
+++ inst/include/dust/random/normal_ziggurat.hpp
@@ -13,15 +13,18 @@ template <typename real_type, typename rng_state_type>
 __host__ __device__
 real_type normal_ziggurat_tail(rng_state_type& rng_state, real_type x1,
                                bool negative) {
+  real_type ret;
   do {
     const auto u1 = random_real<real_type>(rng_state);
     const auto u2 = random_real<real_type>(rng_state);
     const auto x = std::log(u1) / x1;
     const auto y = std::log(u2);
     if (- 2 * y > x * x) {
-      return negative ? x - x1 : x1 - x;
+      ret = negative ? x - x1 : x1 - x;
+      break;
     }
   } while (true);
+  return ret;
 }
 }
 
@@ -40,23 +43,28 @@ real_type random_normal_ziggurat(rng_state_type& rng_state) {
   constexpr size_t n = 256;
   const real_type r = x[1];
 
+  real_type ret;
   do {
     const auto i = (next(rng_state) >> 16) % n;
     const auto u0 = 2 * random_real<real_type>(rng_state) - 1;
     if (std::abs(u0) < y[i]) {
-      return u0 * x[i];
+      ret = u0 * x[i];
+      break;
     }
     if (i == 0) {
-      return normal_ziggurat_tail<real_type>(rng_state, r, u0 < 0);
+      ret = normal_ziggurat_tail<real_type>(rng_state, r, u0 < 0);
+      break;
     }
     const auto z = u0 * x[i];
     const auto f0 = std::exp(-0.5 * (x[i] * x[i] - z * z));
     const auto f1 = std::exp(-0.5 * (x[i + 1] * x[i + 1] - z * z));
     const auto u1 = random_real<real_type>(rng_state);
     if (f1 + u1 * (f0 - f1) < 1.0) {
-      return z;
+      ret = z;
     }
   } while (true);
+  SYNCWARP
+  return ret;
 }
 
 }
