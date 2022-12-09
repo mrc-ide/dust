f04799573d9d24a186716954ea1f84a03e30dafd
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-12-09 15:53:43 +0000

    Test ode_control handling in gpu models

--- tests/testthat/test-interface.R
+++ tests/testthat/test-interface.R
@@ -593,14 +593,6 @@ test_that("ode_control returns NULL for discrete time models", {
 })
 
 
-test_that("error to provide ode control to discrete time models", {
-  res <- dust_example("walk")
-  expect_error(
-    res$new(list(sd = 1), 0, 1, ode_control = dust_ode_control()),
-    "'ode_control' must be NULL for discrete time models")
-})
-
-
 test_that("ode_statistics use is an error for discrete time models", {
   res <- dust_example("walk")
   mod <- res$new(list(sd = 1), 0, 1)
