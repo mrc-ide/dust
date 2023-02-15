88256209377a5086bffeaaa874250d31259f02d2
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-02-15 09:01:39 +0000

    Start generalising logistic

--- inst/examples/ode/logistic.cpp
+++ inst/examples/ode/logistic.cpp
@@ -7,10 +7,9 @@ public:
   using rng_state_type = dust::random::generator<real_type>;
 
   struct shared_type {
-    real_type r1;
-    real_type K1;
-    real_type r2;
-    real_type K2;
+    size_t n;
+    std::vector<real_type> r;
+    std::vector<real_type> K;
   };
 
   logistic(const dust::pars_type<logistic>& pars): shared(pars.shared) {
@@ -19,23 +18,23 @@ public:
   void rhs(real_type t,
            const std::vector<real_type>& y,
            std::vector<real_type>& dydt) const {
-    const real_type N1 = y[0];
-    const real_type N2 = y[1];
-    dydt[0] = shared->r1 * N1 * (1 - N1 / shared->K1);
-    dydt[1] = shared->r2 * N2 * (1 - N2 / shared->K2);
+    for (size_t i = 0; i < shared->n; ++i) {
+      dydt[i] = shared->r[i] * y[i] * (1 - y[i] / shared->K[i]);
+    }
   }
 
   void output(real_type t,
-         const std::vector<real_type>& y,
-         std::vector<real_type>& output) {
-    const real_type N1 = y[0];
-    const real_type N2 = y[1];
-    output[0] = N1 + N2;
+              const std::vector<real_type>& y,
+              std::vector<real_type>& output) {
+    real_type tot = 0;
+    for (size_t i = 0; i < shared->n; ++i) {
+      tot += y[i];
+    }
+    output[0] = tot;
   }
 
   std::vector<real_type> initial(real_type time) {
-    std::vector<real_type> ret = {1, 1};
-    return ret;
+    return std::vector<real_type>(shared->n, 1);
   }
 
   void update_stochastic(real_type t, const std::vector<real_type>& y,
@@ -44,7 +43,7 @@ public:
   }
 
   size_t n_variables() const {
-    return 2;
+    return shared->n;
   }
 
   size_t n_output() const {
@@ -60,22 +59,25 @@ namespace dust {
 template <>
 dust::pars_type<logistic> dust_pars<logistic>(cpp11::list pars) {
   using real_type = logistic::real_type;
-  // [[dust::param(r1, required = TRUE)]]
-  real_type r1 = cpp11::as_cpp<double>(pars["r1"]);
-  // [[dust::param(K1, required = TRUE)]]
-  real_type K1 = cpp11::as_cpp<double>(pars["K1"]);
-  // [[dust::param(r2, required = TRUE)]]
-  real_type r2 = cpp11::as_cpp<double>(pars["r2"]);
-  // [[dust::param(K2, required = TRUE)]]
-  real_type K2 = cpp11::as_cpp<double>(pars["K2"]);
-
-  logistic::shared_type shared{r1, K1, r2, K2};
+  // [[dust::param(r, required = TRUE)]]
+  std::vector<real_type> r = cpp11::as_cpp<std::vector<real_type>>(pars["r"]);
+  // [[dust::param(K, required = TRUE)]]
+  std::vector<real_type> K = cpp11::as_cpp<std::vector<real_type>>(pars["K"]);
+  if (r.size() != K.size()) {
+    cpp11::stop("Expected 'r' and 'K' to have the same size");
+  }
+  const size_t n = r.size();
+  if (n == 0) {
+    cpp11::stop("'r' and 'K' must have length of at least 1");
+  }
+  logistic::shared_type shared{n, r, K};
   return dust::pars_type<logistic>(shared);
 }
 
 template <>
 cpp11::sexp dust_info<logistic>(const dust::pars_type<logistic>& pars) {
-  return cpp11::writable::strings({"N1", "N2"});
+  using namespace cpp11::literals;
+  return cpp11::writable::list({"n"_nm = pars.shared->n});
 }
 
 }
