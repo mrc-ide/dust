40c8e173582be14880c91e80c100b122287b371e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-02-17 15:58:06 +0000

    Test some error creation

--- tests/testthat/test-ode-control.R
+++ tests/testthat/test-ode-control.R
@@ -20,3 +20,25 @@ test_that("can set values into an ode control object", {
          step_size_min = 1e-8, step_size_max = Inf,
          debug_record_step_times = FALSE))
 })
+
+
+## It's likely that we would be better served doing this validation at
+## the point of construction really.
+test_that("Can validate ode control", {
+  ex <- example_logistic()
+  ## Not a great error message, due to the non-scalar return value of
+  ## as_integer
+  expect_error(
+    ex$generator$new(ex$pars, 10, 1,
+                     ode_control = dust_ode_control(max_steps = pi)),
+    "All elements of 'max_steps' must be integer-like")
+  expect_error(
+    ex$generator$new(ex$pars, 10, 1,
+                     ode_control = dust_ode_control(atol = numeric())),
+    "Expected 'atol' to be a scalar value")
+  expect_error(
+    ex$generator$new(ex$pars, 10, 1,
+                     ode_control = dust_ode_control(
+                       debug_record_step_times = logical())),
+    "'debug_record_step_times' must be a non-missing scalar logical")
+})
